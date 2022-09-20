1b437aac879c08c39f2a34de0522f23c219559aa
aparstechteam <79406959+aparstechteam@users.noreply.github.com>
2022-09-20 10:49:52 +0600

    60->50

--- assets/Home.1fa68d10.js
+++ assets/Home.1fa68d10.js
@@ -1,4 +1,4 @@
-import{r as l,o as t,c as s,a as e,b as a,w as c,d as o}from"./index.3bc4993d.js";const r={class:"text-gray-900"},_={class:"container py-10 mx-auto text-center min-h-screen grid place-items-center"},d={class:"max-w-lg rounded-2xl mx-auto p-3 space-y-5"},h=e("h1",{class:"text-2xl"},"Potential Medicalian",-1),m=e("h3",null,"22 Batch",-1),p={key:0,class:"px-3"},x=e("br",null,null,-1),u=e("h2",{class:"text-red-500 font-semibold text-lg"}," Marks Distribution ",-1),g=e("pre",null,`
+import{r as l,o as t,c as s,a as e,b as a,w as c,d as o}from"./index.57f1daf1.js";const r={class:"text-gray-900"},_={class:"container py-10 mx-auto text-center min-h-screen grid place-items-center"},d={class:"max-w-lg rounded-2xl mx-auto p-3 space-y-5"},h=e("h1",{class:"text-2xl"},"Potential Medicalian",-1),m=e("h3",null,"22 Batch",-1),p={key:0,class:"px-3"},x=e("br",null,null,-1),u=e("h2",{class:"text-red-500 font-semibold text-lg"}," Marks Distribution ",-1),g=e("pre",null,`
 Biology-30
 Chemistry-25
 Physics-20
@@ -10,5 +10,5 @@ Negative mark 0.5
 
 Total Question: 100
 Total Marks: 100
-Exam Duration: 60 Minutes
+Exam Duration: 50 Minutes
                     `,-1),k={class:"flex justify-center pb-3 gap-3"},y=o("Go to Exam"),v={key:1,class:"space-y-4"},b=e("p",{class:"text-center text-xl"},[o("\u0985\u0982\u09B6\u0997\u09CD\u09B0\u09B9\u09A3 \u0995\u09B0\u09BE\u09B0 \u09B8\u09AE\u09DF \u09B6\u09C7\u09B7\u0964 "),e("br"),o(" \u09B0\u09C7\u099C\u09BE\u09B2\u09CD\u099F \u099C\u09BE\u09A8\u09A4\u09C7 \u09A8\u09BF\u099A\u09C7\u09B0 \u09AC\u09BE\u099F\u09A8\u09C7 \u0995\u09CD\u09B2\u09BF\u0995 \u0995\u09B0\u09CB")],-1),f=[b],E={__name:"Home",setup(w){const i=new Date("25 September 2022").getTime()<Date.now();return JSON.parse(localStorage.getItem("potentialmedr")),(B,N)=>{const n=l("router-link");return t(),s("div",r,[e("div",_,[e("div",d,[a(n,{to:"/",as:"div",class:"text-center uppercase"},{default:c(()=>[h,m]),_:1}),i?(t(),s("div",v,f)):(t(),s("div",p,[x,u,g,e("div",k,[a(n,{to:"/exam",class:"btn"},{default:c(()=>[y]),_:1})])]))])])])}}};export{E as default};
